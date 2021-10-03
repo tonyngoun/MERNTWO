a3ff16b15b1fd245a7e3c41bd4c5cc8fffbea1cc
Tony <ngount10@hotmail.com>
2021-10-02 17:04:09 -0700

    predict_destructuring

---
+++ JavaScript/Predict_Destructuring.js
@@ -0,0 +1,69 @@
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+//Output  Tesla, Mercedes
+
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+
+//Output  name is redirected by otherName and is not defined
+//Output  Elon
+
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+//Output password 12345 
+// console.log(hashedPassword) hashedPassword is not defined
+
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+//Output false
+//Output true
+
+
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+//Output console.log(key) ; value
+//Output console.log(secondKey) ; [1,5,1,8,3,3]
+//Output console.log(secondKey[0]) ; 1
+//Output console.log(willThisWork) ; 5
