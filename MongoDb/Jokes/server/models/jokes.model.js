a903eb112963badb9d88978fa9098ebdeafc50a6
Tony <ngount10@hotmail.com>
2021-10-31 21:50:31 -0700

    jokes

---
+++ MongoDb/Jokes/server/models/jokes.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const JokeSchema = new mongoose.Schema(
+{
+question: String,
+answer: String,
+},
+{ timestamps: true }
+);
+
+const Joke = mongoose.model("Joke", JokeSchema);
+
+module.exports = Joke;
